ee01fb9a70abef3f65e1b04738fa408a3e906b24
Piotr <pgbercik@gmail.com>
2023-10-03 18:49:19 +0200

    initial commit

---
+++ src/main/java/com/example/demo_hikari_jdbc_oracle/DemoHikariJdbcOracleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_hikari_jdbc_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHikariJdbcOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHikariJdbcOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_hikari_jdbc_oracle/DemoHikariJdbcOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_hikari_jdbc_oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHikariJdbcOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

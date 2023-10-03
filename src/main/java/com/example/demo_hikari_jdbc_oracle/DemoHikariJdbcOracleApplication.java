0b9fc364daa23bac1cbaf45c87098d6474f7c9c9
Piotr <pgbercik@gmail.com>
2023-10-03 18:50:01 +0200

    initial 2

--- src/main/java/com/example/demo_hikari_jdbc_oracle/DemoHikariJdbcOracleApplication.java
+++ src/main/java/com/example/demo_hikari_jdbc_oracle/DemoHikariJdbcOracleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoHikariJdbcOracleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoHikariJdbcOracleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHikariJdbcOracleApplication.class, args);
+    }
 
 }
